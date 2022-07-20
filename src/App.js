28bf3d2b3598c219c8641f6b5002539446173478
igknighton <trent.knighton@gmail.com>
2022-07-19 22:36:27 -0500

    app implementation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
+import logo from './images/logo.svg';
 import './App.css';
-
+import data from './data.json';
+import {useState} from "react";
 function App() {
+
+    const DayOfTheWeek = ({day,amount}) => {
+        const isToday = new Date().toLocaleString("en-US", { "weekday": "short" }).toLowerCase() === day;
+        const barColor = isToday ? 'spendingBarToday': 'spendingBar';
+        const amountHeight = Math.round(amount);
+        const [display,setDisplay] = useState(false);
+
+
+        return <div className="day">
+            <div className="amount" style={{visibility:display ? 'visible':'hidden'}}>${amount}</div>
+            <div
+                className={barColor}
+                onMouseOver={() => setDisplay(true)}
+                onMouseLeave={() => setDisplay(false)}
+                style={{height: `${amountHeight*2}px`}}>
+            </div>
+            <p>{day}</p>
+        </div>
+    }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="balance">
+            <div className="balanceTotal">
+                <p>My balance</p>
+                <h2>$921.48</h2>
+            </div>
+            <img src={logo} className="logo" alt="logo" />
+        </div>
+        <div className="spending">
+            <div className="spendingTitle">
+                <h1>Spending - Last 7 days</h1>
+            </div>
+            <div className="weeklyChart">
+                { data.map(d => <DayOfTheWeek  key={d.day} day={d.day} amount={d.amount}/>) }
+            </div>
+
+            <div className="monthlyTotal">
+                <div className="thisMonth">
+                    <p>Total this month</p>
+                    <h1>$478.33</h1>
+                </div>
+                <div className="lastMonth">
+                    <h4>+2.4%</h4>
+                    <p>from last month</p>
+                </div>
+            </div>
+        </div>
     </div>
   );
 }
